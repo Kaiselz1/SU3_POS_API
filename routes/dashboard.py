a4929c532fb1d8c41f26d874293166f722f4116b
Lor Virak youth <vireakyouth2003@gmail.com>
2025-09-15 19:19:14 +0700

    tesing

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -2,8 +2,14 @@ from app import app, abort
 
 @app.route('/')
 def dashboard():
-    return 'dashboard'
+    return '<h1>dashboard</h1>'
 
 @app.route('/admin')
 def admin():
     abort(403)
+
+@app.route('/list')
+def list():
+    arr = [1,2]
+    arr[3] = 5  # This will raise an IndexError
+    return '<h1>list</h1>'
